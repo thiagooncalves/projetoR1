77d851fe27e828a9a1c9f17331544a4bd19793d6
thiago <goncalvesthiago1@hotmail.com>
2022-08-08 17:34:25 -0300

    update

---
+++ dev-air/dev-air/src/components/Bagagem.jsx
@@ -0,0 +1,10 @@
+export const Bagagem = () => {
+    return (
+        <header>
+            <>
+            <br />
+            </>
+            <h2>Quantidade de bagagens</h2>
+        </header>
+    )
+}
